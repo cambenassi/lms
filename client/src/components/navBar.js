1d06910e5309952cba6068f41cc951424886a954
ccb1139 <calebbergen000@gmail.com>
2022-11-17 12:13:23 -0500

    Update navBar.js

--- client/src/components/navBar.js
+++ client/src/components/navBar.js
@@ -6,16 +6,18 @@ const navbar = () => {
   return (
 
     <nav className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8 '>
-      <div className='relative flex h-16 items-center p-10 list-none'>
-        <li className='m-5'>
-          <Link to='/'>Home</Link>
-        </li>
-        <li className='m-5'>
-          <Link to='/about'>About Us</Link>
-        </li>
+      <div className='sticky'>
+        <div className='flex h-16 items-center justify-center p-10 list-none
+          md:space-x-6 md:space-y-0 '>
+          <li className='m-5 hover:drop-shadow-lg hover:text-slate-500 hover:underline'>
+            <Link to='/'>Home</Link>
+          </li>
+          <li className='m-5 hover:drop-shadow-lg hover:text-slate-500 hover:underline'>
+            <Link to='/about'>About Us</Link>
+          </li>
 
+        </div>
       </div>
-
     </nav>
 
 
