d701a0b6a2e7b3979c0d7f0b35a1416b9beea976
cambenassi <cambenassi@gmail.com>
2022-11-17 17:05:34 -0500

    build notification

--- client/src/App.js
+++ client/src/App.js
@@ -8,11 +8,13 @@ import {
 
 import AboutUs from "./components/aboutUs";
 import Home from "./components/home";
+import Prototype from "./components/prototype"
 
 const App = () => {
   let routes = useRoutes([
     { 	path: "/", element: <Home />},
-	{	path: "/about", element: <AboutUs /> }
+	  {	  path: "/about", element: <AboutUs /> },
+    {   path: "/prototype", element: <Prototype />}
   ]);
   return routes;
 };
---
+++ client/src/components/prototype.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+import Notification from './notification'
+
+export default function prototype() {
+  return (
+    <>
+        <div class="w-full flex justify-center ">
+            <div class="w-[90%] mt-5 p-10 border-2 rounded-lg">
+                <Notification />
+
+            </div>
+        </div>
+    </>
+  )
+}
