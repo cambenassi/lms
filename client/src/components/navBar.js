0a9be62a05ec8632da12c474c4cf6b5afa886793
ccb1139 <calebbergen000@gmail.com>
2022-11-28 14:43:35 -0500

    Added drop down to navbar

--- client/src/components/navBar.js
+++ client/src/components/navBar.js
@@ -1,21 +1,19 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import HeaderDropdown from './headerDropdown'
 
 
 const navbar = () => {
   return (
 
-    <nav className='mx-auto px-2 sm:px-6 lg:px-8 bg-blue-300 w-full'>
-      <div className='sticky'>
-        <div className='flex h-16 items-center justify-center p-10 list-none
+    <nav className='mx-auto grid grid-cols-2 px-2 sm:px-6 lg:px-8 bg-[#0067b1] w-full'>
+      <div className='flex items-center justify-start'>
+        <h1>BLACKBOARD</h1>
+      </div>
+      <div className='sticky justify-end'>
+        <div className='flex h-16 items-center justify-end p-10 list-none
           md:space-x-6 md:space-y-0 '>
-          <li className='m-5 hover:drop-shadow-lg hover:text-slate-500 hover:underline'>
-            <Link to='/'>Home</Link>
-          </li>
-          <li className='m-5 hover:drop-shadow-lg hover:text-slate-500 hover:underline'>
-            <Link to='/about'>About Us</Link>
-          </li>
-
+          <HeaderDropdown></HeaderDropdown>
         </div>
       </div>
     </nav>
