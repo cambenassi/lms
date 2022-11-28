774800129081e002876ef4ad0b3e32d4a965cbaa
ccb1139 <calebbergen000@gmail.com>
2022-11-28 15:26:28 -0500

    header update

--- client/src/components/navBar.js
+++ client/src/components/navBar.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import { Link } from "react-router-dom";
 import HeaderDropdown from './headerDropdown'
-
+import UmlLogo from '../images/uml.png'
 
 const navbar = () => {
   return (
 
-    <nav className='mx-auto grid grid-cols-2 px-2 sm:px-6 lg:px-8 bg-[#0067b1] w-full'>
-      <div className='flex items-center justify-start'>
-        <h1>BLACKBOARD</h1>
+    <nav className='m-auto grid auto-rows-auto grid-flow-row grid-cols-2 px-2 sm:px-6 lg:px-8 bg-[#0067b1] w-full '>
+      <div className='flex items-center'>
+        <div className='grid grid-cols-4'>
+          <div className='flex justify-end mr-3' >
+            <img src={UmlLogo} className='h-16'></img>
+
+          </div>
+          <div className='grid grid-rows-1 col-span-3 '>
+            <p className='font-serif font-bold text-white text-lg'>
+              University of Massachusetts Lowell<br></br>
+              Blackboard Learning Management System
+            </p>
+          </div>
+        </div>
+
+
       </div>
       <div className='sticky justify-end'>
         <div className='flex h-16 items-center justify-end p-10 list-none
