ee44859a87cc79914d0e33c337d1714e7210c97c
ATran2000 <tran.alvinx123@gmail.com>
2022-11-30 01:37:42 -0500

    Made JSON object in server side, but still need to connect it to client side (WorkInProgress)
    Made JSON object which will contain the list of students with their classes and all the notifications that pertain to the classes. Currently a little unsure on how to connect server side code to client side code.

---
+++ server/data/virtualDatabase.js
@@ -0,0 +1,87 @@
+var obj = {
+    "students": [
+        {
+            "name": "Alpha",
+            "id": "01234567",
+            "classes": [
+                {
+                    "className": "PSYC 1010 - Introduction to Psychology",
+                    "professorName": "Beta",
+                    "notifications": [
+                        {
+                            "header": "Chapter 5 Quiz Graded",
+                            "category": "Quiz",
+                            "dueDate": "N/A",
+                            "markAsRead": "Unread",
+                            "timestamp": "2022-11-01 01:00:00.000"
+                        }
+                    ]
+                },
+                {
+                    "className": "LIFE 2050 - Life Sceience II",
+                    "professorName": "Charlie",
+                    "notifications": [
+                        {
+                            "header": "Corrections for Eyesight Lab",
+                            "category": "Paper",
+                            "dueDate": "N/A",
+                            "markAsRead": "Unread",
+                            "timestamp": "2022-11-01 01:00:00.000"
+                        }
+                    ]
+                },
+                {
+                    "className": "COMP 4200 - Software Engineering I",
+                    "professorName": "Delta",
+                    "notifications": [
+                        {
+                            "header": "Chapter 2 Discussion Board Post Assigned",
+                            "category": "Discussion",
+                            "dueDate": "2022-11-19",
+                            "markAsRead": "Unread",
+                            "timestamp": "2022-11-01 01:00:00.000"
+                        }
+                    ]
+                },
+                {
+                    "className": "COMP 4040 - Analysis of Algorithms",
+                    "professorName": "Epsilon",
+                    "notifications": [
+                        {
+                            "header": "Chapter 11 Reading Passed Out",
+                            "category": "Reading",
+                            "dueDate": "2022-12-01",
+                            "markAsRead": "Unread",
+                            "timestamp": "2022-11-30 01:00:00.000"
+                        }
+                    ]
+                }
+            ]
+        }
+    ]
+}
+
+// public functions
+module.exports = {
+    // DESCRIPTION: main public function that gets sent a data request and returns a json variable
+    // PROTOTYPE: sent a json w/ type of request and any data needed to make request
+    getData: async function (dataSlug) {
+        const data = await getDataVirtualDatabase(dataSlug);
+        return data;
+    }
+}
+
+async function getDataVirtualDatabase(dataSlug) {
+    // if statements to determine request type
+    if (dataSlug.requestType == "uniqueNotifications") { // this request will return a list of all notifications for the student
+        return await uniqueNotifications();
+    }
+}
+
+async function uniqueNotifications() {
+    try {
+        
+    } catch (e) {
+        console.error(e);
+    }
+}
--- server/server.js
+++ server/server.js
@@ -1,11 +1,51 @@
 const express = require("express");
+//var cors = require('cors');
 const app = express();
-  
+//var path = require('path');
+
 app.post("/post", (req, res) => {
   console.log("Connected to React");
   res.redirect("/");
 });
-  
+
+// EXPRESS CODE
+// get cors working
+//app.use(cors());
+
+// set up listening on port
 const PORT = process.env.PORT || 8080;
-  
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
+//app.use(express.static('public'));
+
+// API CODE
+//app.use(express.json({ limit: '1mb' }));
+
+/*
+//routing to build folder for index.html
+if (process.env.NODE_ENV === 'production') {
+  // Serve any static files
+  app.use(express.static(path.join(__dirname, '/../client/build')));
+
+  // Handle React routing, return all requests to React app
+  app.get('*', function (req, res) {
+    res.sendFile(path.join(__dirname, '/../client/build', 'index.html'));
+  });
+}
+*/
+
+/*
+// API routing to virtual database
+app.post('/api', async (request, response) => {
+  console.log('I got a request to test!');
+  console.log(request.body);
+  // send request to virtual database, and get constructed object back
+  const data = await virtualDatabase.getData(request.body);
+  // return data
+  response.json({
+    status: 'success',
+    message: data,
+  });
+  console.log("Returned data object!");
+  console.log(data);
+});
+*/
