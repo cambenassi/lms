44d59ade6f8a8ad7d7320153a49ddd9e411fac72
swegroup81 <117475575+swegroup81@users.noreply.github.com>
2022-12-05 14:59:10 -0500

    Update server.js

--- server/server.js
+++ server/server.js
@@ -14,7 +14,10 @@ app.use(cors());
 
 // set up listening on port
 const PORT = process.env.PORT || 8080;
-app.listen(PORT, console.log(`Server started on port ${PORT}`));
+//app.listen(PORT, console.log(`Server started on port ${PORT}`));
+app.listen(process.env.PORT || 8080, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
 
 // API CODE
 app.use(express.json({ limit: '1mb' }));
